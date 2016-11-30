6c8492ec37251b127cd7f0c33c301a4e99e645db
BerniceChua <Bernice.Chua.415@gmail.com>
2016-11-30 15:53:23 -0800

    Add spaceship movements.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,8 +15,8 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
 		GetComponent<Rigidbody>().velocity = movement * speed;
 
 		GetComponent<Rigidbody>().position = new Vector3 (
