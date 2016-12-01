c2283f549e1766e6b83bda0b9d172a5301448357
BerniceChua <Bernice.Chua.415@gmail.com>
2016-11-30 18:33:38 -0800

    Add asteroid and collision detection.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Boundary") {
+			return;
+		}
+
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
