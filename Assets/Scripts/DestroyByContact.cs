09555fa9714b90c4b221bd2f2cf84342c9eaca25
BerniceChua <Bernice.Chua.415@gmail.com>
2016-12-04 23:33:21 -0800

    Add loop so hazards spawn infinitely.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject asteroidExplosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary") {
+		if (other.tag == "Invisible Boundary") {
 			return;
+		}
+
+		Instantiate(asteroidExplosion, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			//GameController.GameOver ();
 		}
 
 		Destroy(other.gameObject);
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButton("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
 			// GameObject clone = 
-				Instantiate (shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+				Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
 		}
 	}
 
