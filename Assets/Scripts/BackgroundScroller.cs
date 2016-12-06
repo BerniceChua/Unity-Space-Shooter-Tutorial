caa2c260f23971ca54ba2e9ae4610fea547588fa
BerniceChua <Bernice.Chua.415@gmail.com>
2016-12-06 11:33:55 -0800

    Add scrolling background.

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSizeZAxis;
+
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZAxis);
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
