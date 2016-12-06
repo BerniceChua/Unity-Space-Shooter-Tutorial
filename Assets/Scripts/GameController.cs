905f1e84279b8b655df43e3888f67c2d3973723e
BerniceChua <Bernice.Chua.415@gmail.com>
2016-12-06 10:37:50 -0800

    Add array with multiple asteroids designs.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValues;
 	public int hazardCount;
     public float spawnDelay;
@@ -42,6 +42,7 @@ public class GameController : MonoBehaviour {
         yield return new WaitForSeconds(startWait);
         while (!gameOver) {
             for (int i = 0; i < hazardCount; i++) {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
