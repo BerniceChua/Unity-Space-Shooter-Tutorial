b151dda9af9091559dbc3c365fcd6f6913f6274c
BerniceChua <Bernice.Chua.415@gmail.com>
2016-12-06 16:31:20 -0800

    Add enemy ship movements.

---
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour {
+
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public float smoothing;
+    public float dodge;
+    public float tilt;
+    public Boundary boundary;
+
+    private float targetManeuver;
+    private Rigidbody rb;
+    private float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+	}
+	
+    IEnumerator Evade() {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+        while (true) {
+            targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+
+        rb.position = new Vector3(Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax));
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
